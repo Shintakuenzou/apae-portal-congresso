412ac55b0e3c16dc442b706ef84b267109b39e09
felipe enzou <felipe.enzou@gmail.com>
2026-03-09 16:35:32 -0300

    correção da importação da tipagem na auth context

--- src/types/auth-context-type.tsx
+++ src/types/auth-context-type.tsx
@@ -1,10 +1,10 @@
 import type { User } from "./user";
 
-export interface AuthContextType {
+export type AuthContextType = {
   user: User | null;
   isLoading: boolean;
   login: (cpf: string, password: string) => Promise<void>;
   logout: () => void;
   isAuthenticated: boolean;
   updateUser: (data: Partial<User>) => void;
-}
+};
