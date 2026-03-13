42da109feec4c41acd0af6e5b618a0afa7e5f08c
felipe enzou <felipe.enzou@gmail.com>
2026-03-13 16:54:14 -0300

    ajustnado para consulta produção

--- src/services/api-root.ts
+++ src/services/api-root.ts
@@ -3,7 +3,7 @@ import CryptoJs from "crypto-js";
 import axios, { type AxiosRequestHeaders, type InternalAxiosRequestConfig } from "axios";
 
 // URL do Fluig (usada para assinar requisições OAuth em desenvolvimento)
-const FLUIG_BASE_URL = import.meta.env.VITE_BASE_URL as string;
+const FLUIG_BASE_URL = import.meta.env.VITE_BASE_URL_PROD as string;
 
 // ✅ Determina a base URL dependendo do ambiente
 const getBaseURL = () => {
